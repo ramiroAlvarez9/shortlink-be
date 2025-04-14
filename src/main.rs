bca35ee29ce7d3ec3617e0fad949a83ed51285eb
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2025-04-14 02:11:23 -0300

    feat: deleting the unwrap_or_else

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,16 @@ use tokio_postgres::{Error, NoTls};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let host = env::var("SERVER_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
-    let port = env::var("SERVER_PORT").unwrap_or_else(|_| "9090".to_string());
+    let host = env::var("SERVER_HOST").unwrap();
+    let port = env::var("SERVER_PORT").unwrap();
     let _db_connection_string = format!(
         "host={} port={} user={} password={} dbname={}",
-        env::var("DB_HOST").unwrap_or_else(|_| "localhost".to_string()),
-        env::var("DB_PORT").unwrap_or_else(|_| "111111".to_string()),
-        env::var("DB_USER").unwrap_or_else(|_| "postgres".to_string()),
-        env::var("DB_PASSWORD").unwrap_or_else(|_| "1111111".to_string()),
-        env::var("DB_NAME").unwrap_or_else(|_| "1111111".to_string())
+        env::var("DB_HOST").unwrap(),
+        env::var("DB_PORT").unwrap(),
+        env::var("DB_USER").unwrap(),
+        env::var("DB_PASSWORD").unwrap(),
+        env::var("DB_NAME").unwrap()
+        
     );
 
     let (client, connection) = tokio_postgres::connect(&_db_connection_string, NoTls).await?;
